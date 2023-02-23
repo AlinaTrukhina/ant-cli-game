bc357447295ce05e03ad1a86ab4f01dc40911f59
AlinaTrukhina <alina.trukhina@gmail.com>
2023-02-23 16:20:05 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+import inquirer from "inquirer";
+
+console.log(chalk.cyanBright('hello world'));
