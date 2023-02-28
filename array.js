656781adef6d995e2605f73aff4613d91ac16440
AlinaTrukhina <alina.trukhina@gmail.com>
2023-02-27 18:46:48 -0600

    convert to single dimension array

--- array.js
+++ array.js
@@ -1,5 +1,9 @@
 let arr1 = [' hearts', ' diamonds', ' spades', ' clubs'];
 let arr2 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
 let newarr = arr2.map((x) => arr1.map(y => x + y));
-
+let finalArr = [];
+for (let i = 0; i < newarr.length; i++){
+    finalArr = finalArr.concat(newarr[i]);
+}
 console.log(newarr);
+console.log(finalArr);
