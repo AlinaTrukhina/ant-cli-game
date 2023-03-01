41a3a60b51c730de836088052d05e84b2ac0e5aa
AlinaTrukhina <alina.trukhina@gmail.com>
2023-02-28 20:30:22 -0600

    modify blackjack

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ import figlet from "figlet";
 import gradient from "gradient-string";
 import inquirer from "inquirer";
 import { createSpinner } from "nanospinner";
+import chalkPipe from 'chalk-pipe';
 
 let playerName;
-let target;
-
-const COLORS_ARRAY = ['black', 'brown' ,'gold', 'gray', 'green', 'magenta', 'orange', 'red', 'white', 'yellow', 'blue'];
 
 const sleep = (ms = 1500) => new Promise((r) => setTimeout(r, ms));
 
@@ -163,102 +161,108 @@ function palindrome(palindromeQ) {
 
 // color guessing game
 async function colorGame() {
+    let target;
+
+    const COLORS_ARRAY = ['black', 'brown' ,'gold', 'gray', 'green', 'magenta', 'orange', 'red', 'white', 'yellow', 'blue'];
     
     const targetIndex = Math.floor(Math.random() * COLORS_ARRAY.length);
     target = COLORS_ARRAY[targetIndex];
 
-    await askColor();
-}
+    async function askColor() {
+        const answers = await inquirer.prompt({
+            name: 'color_guess',
+            type: 'list',
+            message: `I am thinking of one of these colors. 
+            What color am I thinking of?`,
+            choices: ['black', 'brown' ,'gold', 'gray', 'green', 'magenta', 
+            'orange', 'red', 'white', 'yellow', 'blue']
+        });
+        let colored = chalkPipe(answers.color_guess);
+        return chalkPipe(answers.color_guess)(handleColorAnswer(answers.color_guess));
+    }
 
-async function askColor() {
-    const answers = await inquirer.prompt({
-        name: 'color_guess',
-        type: 'list',
-        message: `I am thinking of one of these colors. 
-        What color am I thinking of?`,
-        choices: ['black', 'brown' ,'gold', 'gray', 'green', 'magenta', 
-        'orange', 'red', 'white', 'yellow', 'blue']
-    });
-    let colored = chalkPipe(answers.color_guess);
-    return chalkPipe(answers.color_guess)(handleColorAnswer(answers.color_guess));
-}
-
-async function handleColorAnswer(guess) {
-    const spinner = createSpinner('Checking...').start();
-    await sleep();
-    
-    if (guess > target) {
-        spinner.error({ text: 'Your color guess is alphabetically higher than my color.'});
+    async function handleColorAnswer(guess) {
+        const spinner = createSpinner('Checking...').start();
+        await sleep();
         
-        await askColor();
+        if (guess > target) {
+            spinner.error({ text: 'Your color guess is alphabetically higher than my color.'});
+            
+            await askColor();
+        }
+        else if (guess < target) {
+            spinner.error({ text: 'Your color guess is alphabetically lower than my color.'});
+            await askColor();
+        }
+        else {
+            spinner.success({ text: 'You got it right!'});
+            await askPlayAgain('color');
+        }
     }
-    else if (guess < target) {
-        spinner.error({ text: 'Your color guess is alphabetically lower than my color.'});
-        await askColor();
-    }
-    else {
-        spinner.success({ text: 'You got it right!'});
-        await askPlayAgain('color');
-    }
+
+    await askColor();
 }
 
 
 // blackjack game
 function blackjackGame() {
 let cards = [
-    { card: 'one hearts', value: 1 },
-    { card: 'one diamonds', value: 1 },
-    { card: 'one spades', value: 1 },
-    { card: 'one clubs', value: 1 },
-    { card: 'two hearts', value: 2 },
-    { card: 'two diamonds', value: 2 },
-    { card: 'two spades', value: 2 },
-    { card: 'two clubs', value: 2 },
-    { card: 'three hearts', value: 3 },
-    { card: 'three diamonds', value: 3 },
-    { card: 'three spades', value: 3 },
-    { card: 'three clubs', value: 3 },
-    { card: 'four hearts', value: 4 },
-    { card: 'four diamonds', value: 4 },
-    { card: 'four spades', value: 4 },
-    { card: 'four clubs', value: 4 },
-    { card: 'five hearts', value: 5 },
-    { card: 'five diamonds', value: 5 },
-    { card: 'five spades', value: 5 },
-    { card: 'five clubs', value: 5 },
-    { card: 'six hearts', value: 6 },
-    { card: 'six diamonds', value: 6 },
-    { card: 'six spades', value: 6 },
-    { card: 'six clubs', value: 6 },
-    { card: 'seven hearts', value: 7 },
-    { card: 'seven diamonds', value: 7 },
-    { card: 'seven spades', value: 7 },
-    { card: 'seven clubs', value: 7 },
-    { card: 'eight hearts', value: 8 },
-    { card: 'eight diamonds', value: 8 },
-    { card: 'eight spades', value: 8 },
-    { card: 'eight clubs', value: 8 },
-    { card: 'nine hearts', value: 9 },
-    { card: 'nine diamonds', value: 9 },
-    { card: 'nine spades', value: 9 },
-    { card: 'nine clubs', value: 9 },
-    { card: 'ten hearts', value: 10 },
-    { card: 'ten diamonds', value: 10 },
-    { card: 'ten spades', value: 10 },
-    { card: 'ten clubs', value: 10 },
-    { card: 'jack hearts', value: 10 },
-    { card: 'jack diamonds', value: 10 },
-    { card: 'jack spades', value: 10 },
-    { card: 'jack clubs', value: 10 },
-    { card: 'queen hearts', value: 10 },
-    { card: 'queen diamonds', value: 10 },
-    { card: 'queen spades', value: 10 },
-    { card: 'queen clubs', value: 10 },
-    { card: 'king hearts', value: 10 },
-    { card: 'king diamonds', value: 10 },
-    { card: 'king spades', value: 10 },
-    { card: 'king clubs', value: 10 },
-    { card: 'ace', value: 11 }
+    { card: '1 of hearts', value: 1 },
+    { card: '1 of diamonds', value: 1 },
+    { card: '1 of spades', value: 1 },
+    { card: '1 of clubs', value: 1 },
+    { card: '2 of hearts', value: 2 },
+    { card: '2 of diamonds', value: 2 },
+    { card: '2 of spades', value: 2 },
+    { card: '2 of clubs', value: 2 },
+    { card: '3 of hearts', value: 3 },
+    { card: '3 of diamonds', value: 3 },
+    { card: '3 of spades', value: 3 },
+    { card: '3 of clubs', value: 3 },
+    { card: '4 of hearts', value: 4 },
+    { card: '4 of diamonds', value: 4 },
+    { card: '4 of spades', value: 4 },
+    { card: '4 of clubs', value: 4 },
+    { card: '5 of hearts', value: 5 },
+    { card: '5 of diamonds', value: 5 },
+    { card: '5 of spades', value: 5 },
+    { card: '5 of clubs', value: 5 },
+    { card: '6 of hearts', value: 6 },
+    { card: '6 of diamonds', value: 6 },
+    { card: '6 of spades', value: 6 },
+    { card: '6 of clubs', value: 6 },
+    { card: '7 of hearts', value: 7 },
+    { card: '7 of diamonds', value: 7 },
+    { card: '7 of spades', value: 7 },
+    { card: '7 of clubs', value: 7 },
+    { card: '8 of hearts', value: 8 },
+    { card: '8 of diamonds', value: 8 },
+    { card: '8 of spades', value: 8 },
+    { card: '8 of clubs', value: 8 },
+    { card: '9 of hearts', value: 9 },
+    { card: '9 of diamonds', value: 9 },
+    { card: '9 of spades', value: 9 },
+    { card: '9 of clubs', value: 9 },
+    { card: '10 of hearts', value: 10 },
+    { card: '10 of diamonds', value: 10 },
+    { card: '10 of spades', value: 10 },
+    { card: '10 of clubs', value: 10 },
+    { card: 'Jack of hearts', value: 10 },
+    { card: 'Jack of diamonds', value: 10 },
+    { card: 'Jack of spades', value: 10 },
+    { card: 'Jack of clubs', value: 10 },
+    { card: 'Queen of hearts', value: 10 },
+    { card: 'Queen of diamonds', value: 10 },
+    { card: 'Queen of spades', value: 10 },
+    { card: 'Queen of clubs', value: 10 },
+    { card: 'King of hearts', value: 10 },
+    { card: 'King of diamonds', value: 10 },
+    { card: 'King of spades', value: 10 },
+    { card: 'King of clubs', value: 10 },
+    { card: 'Ace', value: 11 },
+    { card: 'Ace', value: 11 },
+    { card: 'Ace', value: 11 },
+    { card: 'Ace', value: 11 }
 ];
 
 let playerCards = [];
@@ -279,28 +283,31 @@ let dealerPoints = 0;
 
 // after every turn, call fn checkScores()
 
-// get random card helper function
-function getCard(who) {
-    const cardIndex = Math.floor(Math.random() * cards.length);
-    const newCard = cards[cardIndex];
-    cards.splice(cardIndex, 1);
-    who.push(newCard);
+    // get random card helper function
+    function getCard(who) {
+        const cardIndex = Math.floor(Math.random() * cards.length);
+        const newCard = cards[cardIndex];
+        cards.splice(cardIndex, 1);
+        who.push(newCard);
 
-    playerPoints = playerCards.reduce((a, b) => a + b.value, 0,);
-    dealerPoints = dealerCards.reduce((a, b) => a + b.value, 0,);
-    
-    return who;
-}
+        playerPoints = playerCards.reduce((a, b) => a + b.value, 0,);
+        dealerPoints = dealerCards.reduce((a, b) => a + b.value, 0,);
+        
+        return who;
+    }
 
-async function dealBlackjack() {
-    await (getCard(playerCards));
-    await (getCard(playerCards));
+    async function dealBlackjack() {
+        // const spinner = await createSpinner('Dealing Cards...').start();
+        // await sleep();
+        await console.clear();
+        await (getCard(playerCards));
+        await (getCard(playerCards));
 
-    await (getCard(dealerCards));
-    await (getCard(dealerCards));
+        await (getCard(dealerCards));
+        await (getCard(dealerCards));
 
-    await checkScores();
-}
+        await checkScores();
+    }
 
     async function hitOrStay() {
         const answers = await inquirer.prompt({
@@ -311,6 +318,27 @@ async function dealBlackjack() {
         });
         if (answers.hit_stay == 'Hit me!') {
             await getCard(playerCards);
+            await console.clear();
+            if (playerPoints > 21) {
+                console.log('Your cards:');
+                console.log('---------');
+                for (let card of playerCards) {
+                    console.log(card.card);
+                    console.log('---------');
+                }
+                console.log('Your points:', playerPoints);
+                console.log('');
+                console.log('Dealer cards:');
+                console.log('---------');
+                for (let card of dealerCards) {
+                    console.log(card.card);
+                    console.log('---------');
+                }
+                console.log('Dealer points:', dealerPoints);
+                console.log('');
+                console.log('the house wins');
+                await askPlayAgain('blackjack');
+            }
             if (dealerPoints < 21) {
                 await getCard(dealerCards);
             }
@@ -321,29 +349,43 @@ async function dealBlackjack() {
         }
     }
 
-    async function checkScores() {
-        console.log(playerCards, 'your points:', playerPoints);
-        console.log(dealerCards, 'dealer points:', dealerPoints);
+    function checkScores() {
+        console.clear();
+        console.log('Your cards:');
+        console.log('---------');
+        for (let card of playerCards) {
+            console.log(card.card);
+            console.log('---------');
+        }
+        console.log('Your points:', playerPoints);
+        console.log('');
+        console.log('Dealer cards:');
+        console.log('---------');
+        for (let card of dealerCards) {
+            console.log(card.card);
+            console.log('---------');
+        }
+        console.log('Dealer points:', dealerPoints);
+        console.log('');
 
         if (playerPoints == 21 && dealerPoints == 21) {
             console.log('draw');
-            await askPlayAgain('blackjack');
+            askPlayAgain('blackjack');
         } else if (dealerPoints > 21 ) {
             console.log('you win');
-            await askPlayAgain('blackjack');
+            askPlayAgain('blackjack');
         } else if (playerPoints == 21 && dealerPoints < 21) {
             console.log('dealer draws');
-            await (getCard(dealerCards));
-            await checkScores();
+            (getCard(dealerCards));
+            checkScores();
         } else if (playerPoints < 21 && dealerPoints < 21) {
-            await hitOrStay();
-            await (getCard(dealerCards));
+            hitOrStay();
         } else if (playerPoints < 21 && dealerPoints == 21) {
             console.log('the house wins');
-            await askPlayAgain('blackjack');
+            askPlayAgain('blackjack');
         } else {
             console.log('the house wins');
-            await askPlayAgain('blackjack');
+            askPlayAgain('blackjack');
         }
     }
 
